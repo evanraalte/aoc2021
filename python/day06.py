3c3bbc40b1220fe22cec1d82dfd5ded9855205a1
Erik van Raalte <erik.vanraalte94@gmail.com>
2021-12-06 09:27:11 +0100

    adds day6

---
+++ python/day06.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+
+@lru_cache
+def reproduce(state, num_days, num_fish=1):
+    while num_days > 0:
+        num_days -= 1
+        if state == 0:
+            num_fish += reproduce(state=8, num_days=num_days)
+            state = 6
+        else:
+            state -= 1
+    return num_fish
+
+
+def calc_fishes(days, initial_fishes):
+    return sum(reproduce(f, days) for f in initial_fishes)
+
+
+initial_fishes = [int(x) for x in open("assets/day06.txt").read().split(",")]
+
+print(f"Part A: {calc_fishes(80, initial_fishes)}")
+print(f"Part B: {calc_fishes(256, initial_fishes)}")
