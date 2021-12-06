ede070110a38ebb730f1a30999fea70024e97638
Erik van Raalte <erik.vanraalte94@gmail.com>
2021-12-06 10:02:22 +0100

    improves cache size

--- python/day06.py
+++ python/day06.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 
 
-@lru_cache
+@lru_cache(maxsize=None)
 def reproduce(state, num_days, num_fish=1):
     while num_days > 0:
         num_days -= 1
@@ -17,7 +17,7 @@ def calc_fishes(days, initial_fishes):
     return sum(reproduce(f, days) for f in initial_fishes)
 
 
-initial_fishes = [int(x) for x in open("assets/day06.txt").read().split(",")]
+initial_fishes = (int(x) for x in open("assets/day06.txt").read().split(","))
 
 print(f"Part A: {calc_fishes(80, initial_fishes)}")
 print(f"Part B: {calc_fishes(256, initial_fishes)}")
