1fdc07c5c279e678a8f37a7c6de777bc226e7f72
erva <erik.van.raalte@nordicsemi.no>
2021-12-06 13:52:10 +0100

    bit cleaner

--- python/day06.py
+++ python/day06.py
@@ -4,8 +4,8 @@ from collections import deque
 def calc_fishes(days, initial_fishes):
     table = deque(initial_fishes.count(k) for k in range(0, 9))
     for _ in range(0, days):
+        table[7] += table[0]
         table.rotate(-1)
-        table[6] += table[8]
     return sum(table)
 
 
