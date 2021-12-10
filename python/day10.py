fad43aed2fb45be10f4a021e538efb1fbdcb3651
Erik van Raalte <erik.vanraalte94@gmail.com>
2021-12-10 08:15:37 +0100

    adds progress day10

--- python/day10.py
+++ python/day10.py
@@ -26,7 +26,9 @@ class InvalidLineException(Exception):
 def check_chunk(chunk, remainder):
     closed = False
     while not closed:
-        if remainder == "":
+        if chunk in chunk_end.values():
+            raise InvalidLineException(chunk, "")
+        elif remainder == "":
             raise LineNotFinishedException()
         elif remainder[0] in "{([<":
             remainder = check_chunk(
@@ -42,19 +44,21 @@ def check_chunk(chunk, remainder):
     return remainder[1:]
 
 
-data = open("assets/day10-sample.txt").read().split("\n")
+data = open("assets/day10.txt").read().split("\n")
 total = 0
 for line in data:
+    # line = "()())"
     evaluate_line = True
-    rem = line[1:]
+    rem = line[:]
     while evaluate_line and rem:  # while the whole thing is not consumed
         try:
-            rem = check_chunk(line[0], rem)
+            rem = check_chunk(rem[0], rem[1:])
         except LineNotFinishedException:
+            print(f"during checking {line} -> Line incomplete!")
             evaluate_line = False
         except InvalidLineException as e:
             evaluate_line = False
-            print(f"during checking {line}")
-            print(f"expected {e.expected}, found {e.found} -> +{score[e.found]}")
+            print(f"during checking {line} -> Found error!")
+            print(f"expected '{e.expected}', found '{e.found}' -> +{score[e.found]}")
             total += score[e.found]
 print(f"Part A: {total}")
