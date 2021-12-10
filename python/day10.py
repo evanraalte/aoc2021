9d0888af901de05a8533e55c4828e9e74c9832e6
Erik van Raalte <erik.vanraalte94@gmail.com>
2021-12-10 07:42:22 +0100

    progress with exception handling

--- python/day10.py
+++ python/day10.py
@@ -17,36 +17,44 @@ class LineNotFinishedException(Exception):
     pass
 
 
+class InvalidLineException(Exception):
+    def __init__(self, found, expected) -> None:
+        self.expected = expected
+        self.found = found
+
+
 def check_chunk(chunk, remainder):
     closed = False
-    msg = None
     while not closed:
         if remainder == "":
-            return (msg, remainder)
+            raise LineNotFinishedException()
         elif remainder[0] in "{([<":
-            msg, remainder = check_chunk(
+            remainder = check_chunk(
                 remainder[0], remainder[1:]
             )  # solve a new chunk up the stack
-            if msg is not None:
-                return (msg, None)  # propagate error down the stack
             continue
         elif remainder[0] == chunk_end[chunk]:
             closed = True
         else:
-            msg = (remainder[0], chunk_end[chunk])
-            break
-    return (msg, remainder[1:])
+            found = remainder[0]
+            expected = chunk_end[chunk]
+            raise InvalidLineException(found, expected)
+    return remainder[1:]
 
 
 data = open("assets/day10-sample.txt").read().split("\n")
 total = 0
 for line in data:
-    print(f"checking {line}")
+    evaluate_line = True
     rem = line[1:]
-    while rem:  # while the whole thing is not consumed
-        msg, rem = check_chunk(line[0], rem)
-        if msg is not None:
-            found, expected = msg
-            print(f"{found} -> +{score[found]}")
-            total += score[found]
+    while evaluate_line and rem:  # while the whole thing is not consumed
+        try:
+            rem = check_chunk(line[0], rem)
+        except LineNotFinishedException:
+            evaluate_line = False
+        except InvalidLineException as e:
+            evaluate_line = False
+            print(f"during checking {line}")
+            print(f"expected {e.expected}, found {e.found} -> +{score[e.found]}")
+            total += score[e.found]
 print(f"Part A: {total}")
