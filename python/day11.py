1743fb122e7cae969051e9a3f56e27a4355e84cc
Erik van Raalte <erik.vanraalte94@gmail.com>
2021-12-11 15:52:23 +0100

    can't flash twice

--- python/day11.py
+++ python/day11.py
@@ -7,22 +7,21 @@ all_flashed = False
 step = 0
 while not all_flashed:
     step += 1
-    flashed_octopuses = set()
     for location in octopuses.keys():
         octopuses[location] += 1
-
+    flashed_octopuses = 0
     while len(flashing_octopuses := list(filter(lambda v: v[1] == 10, octopuses.items()))):
         for location, octopus in flashing_octopuses:
-            flashed_octopuses.add(location)  # add it to already exploded octopuses
+            flashed_octopuses += 1
             for dx, dy in itertools.product([-1, 0, 1], repeat=2):
                 n = (location[0] + dx, location[1] + dy)
-                if octopuses.get(n) and octopuses[n] != 10 and n not in flashed_octopuses:
+                if octopuses.get(n) and octopuses[n] != 10:  # and n not in flashed_octopuses:
                     octopuses[n] += 1
             octopuses[location] = 0
-    total += len(flashed_octopuses)
+    total += flashed_octopuses
     if step == 100:
         print("PartA: ", total)
-    if len(flashed_octopuses) == len(octopuses):
+    if flashed_octopuses == len(octopuses):
         print("PartB: ", step)
         all_flashed = True
     pass
