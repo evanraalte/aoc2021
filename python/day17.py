a50ba5b193d882b4b2ac0403e018878850d2dfa6
Erik van Raalte <erik.vanraalte94@gmail.com>
2021-12-17 11:29:39 +0100

    improve ranges

--- python/day17.py
+++ python/day17.py
@@ -1,14 +1,16 @@
+from math import sqrt
+
 # data = "target area: x=20..30, y=-10..-5"
-data ="target area: x=117..164, y=-140..-89"
+data = "target area: x=117..164, y=-140..-89"
 x_area = tuple(map(int, data.split()[2].split("x=")[1].replace(",", "").split("..")))
 y_area = tuple(map(int, data.split()[3].split("y=")[1].split("..")))
 pass
 
 
 y_max = 0
-init_vals = []
-for y_start in range(-500,500):
-    for x_start in range(-500,500):
+init_vals = 0
+for y_start in range(y_area[0], -y_area[0]):
+    for x_start in range(int((-1 + sqrt(937)) / 2), x_area[1] + 1):
         x_vel = x_start
         y_vel = y_start
         x_pos = 0
@@ -29,16 +31,16 @@ for y_start in range(-500,500):
             in_box = (x_area[1] >= x_pos >= x_area[0]) and (y_area[1] >= y_pos >= y_area[0])
 
             too_far_x = ((x_pos < x_area[0]) and x_vel <= 0) or ((x_pos > x_area[1]) and x_vel >= 0)
-            too_far_y = ((y_pos < y_area[0]) and y_vel <= 0)#  or ((y_pos > y_area[0]) and y_vel >= 0)
+            too_far_y = (y_pos < y_area[0]) and y_vel <= 0
             too_far = too_far_y or too_far_x
             done = too_far or in_box
             # print(f"{too_far_x=} {too_far_y=}, {too_far=}")
         if in_box:
-            print(f"{x_start=}, {y_start=}: {y_pos_max}")
-            init_vals.append((x_start,y_start))
+            # print(f"{x_start=}, {y_start=}: {y_pos_max}")
+            init_vals += 1
             if y_pos_max > y_max:
                 y_max = y_pos_max
 
 
 print(y_max)
-print(len(init_vals))
+print(init_vals)
