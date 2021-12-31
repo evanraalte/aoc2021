d9c1a144aebe1f2d2a5e1320b46565ce263f0186
van Raalte <erik.van.raalte@nordicsemi.no>
2021-12-31 15:43:12 +0100

    shorten solution a bit

--- python/day24.py
+++ python/day24.py
@@ -4,34 +4,29 @@ program = open("assets/day24.txt").read().split('\n')
 stack = []
 idx = 0
 checklist = []
-for i in range(0,14):
-    a = int(program[i*18+4].split()[2])
-    b = int(program[i*18+5].split()[2])
-    c = int(program[i*18+15].split()[2])
+for i in range(0,18*14,18):
+    a = int(program[i+4].split()[2])
+    b = int(program[i+5].split()[2])
+    c = int(program[i+15].split()[2])
     if a == 1: #push
         stack.append((idx,c))
     elif a == 26: # pop
         tmp = stack.pop()
         checklist.append(((tmp[0], tmp[1]+b), idx))
     idx += 1
-
-I = [None]*14
+Imax = [None]*14
+Imin = [None]*14
 for (i1, v), i2 in checklist:
     if v < 0:
-        I[i1] = 9
-        I[i2] = 9 + v
+        Imax[i1] = 9
+        Imax[i2] = 9 + v
+        Imin[i1] = 1 - v
+        Imin[i2] = 1
     else:
-        I[i1] = 9 - v
-        I[i2] = 9
-print("".join(map(str,I)))
-
-I = [None]*14
-for (i1, v), i2 in checklist:
-    if v < 0:
-        I[i1] = 1 - v
-        I[i2] = 1
-    else:
-        I[i1] = 1
-        I[i2] = 1 + v
-print("".join(map(str,I)))
+        Imax[i1] = 9 - v
+        Imax[i2] = 9
+        Imin[i1] = 1
+        Imin[i2] = 1 + v
+print("".join(map(str,Imax)))
+print("".join(map(str,Imin)))
 pass
