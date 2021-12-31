d92904f7f5908e35617413644380d413d5ced871
van Raalte <erik.van.raalte@nordicsemi.no>
2021-12-31 15:25:35 +0100

    adds day 24

--- python/day24.py
+++ python/day24.py
@@ -1,56 +1,37 @@
 from collections import defaultdict, Counter
-
+import time
 program = open("assets/day24.txt").read().split('\n')
+stack = []
+idx = 0
+checklist = []
+for i in range(0,14):
+    a = int(program[i*18+4].split()[2])
+    b = int(program[i*18+5].split()[2])
+    c = int(program[i*18+15].split()[2])
+    if a == 1: #push
+        stack.append((idx,c))
+    elif a == 26: # pop
+        tmp = stack.pop()
+        checklist.append(((tmp[0], tmp[1]+b), idx))
+    idx += 1
 
-
-
-def parse_instruction(line,data, input):
-    next_input = False
-    splitted_line = line.split()
-    if len(splitted_line) == 2:
-        cmd, a = splitted_line
-        b = None
+I = [None]*14
+for (i1, v), i2 in checklist:
+    if v < 0:
+        I[i1] = 9
+        I[i2] = 9 + v
     else:
-        cmd, a, b = splitted_line
-    try:
-        data_b = int(b)
-    except ValueError:
-        data_b = data[b]
-    except TypeError:
-        pass
-    match cmd:
-        case "inp": 
-            data[a] = input
-            next_input = True
-        case "add":
-            data[a] = data[a] + data_b
-        case "mul":
-            data[a] = data[a] * data_b
-        case "div":
-            data[a] = data[a] // data_b
-        case "mod":
-            data[a] = data[a] % data_b
-        case "eql":
-            data[a] = int(data[a] == data_b)
-    return next_input
+        I[i1] = 9 - v
+        I[i2] = 9
+print("".join(map(str,I)))
 
-        
-nums = (str(i).zfill(14) for i in range(11111111111111,99999999999999) if '0' not in str(i).zfill(14))
-x = defaultdict(lambda: [])
-for num in nums:
-    # num = str(13579246899999)
-    inputs = list(num)
-    inputs.append(0)
-    idx = 0
-    data = defaultdict(lambda: 0, {})
-    for line in program:
-        next_inp = parse_instruction(line,data,int(inputs[idx]))
-        # print(data)
-        if next_inp:
-            idx += 1
-    # print(num, " ", data['z'])
-    x[data['z']].append(num)
-    if data['z'] == 0:
-        print(num)
-        break
-# print(data)
+I = [None]*14
+for (i1, v), i2 in checklist:
+    if v < 0:
+        I[i1] = 1 - v
+        I[i2] = 1
+    else:
+        I[i1] = 1
+        I[i2] = 1 + v
+print("".join(map(str,I)))
+pass
